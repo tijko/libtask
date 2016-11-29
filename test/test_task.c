b88eec9c6ae599314137d29e63edb447222a23b7
Tim Konick <konick781@gmail.com>
2016-11-28 23:13:07 -0800

    Include comparison against procfs

--- test/test_task.c
+++ test/test_task.c
@@ -19,9 +19,12 @@ void test_write(void)
 
     int test_io_file_write_fd = open(TEST_FILE, O_WRONLY | O_CREAT);
 
-    printf("Attempting to write <%d> bytes\n", TEST_DATA_SIZE); 
     int wbytes = write(test_io_file_write_fd, test_input, TEST_DATA_SIZE);
-    printf("<%d> written\n", wbytes);
+    if (wbytes < 0) {
+        perror("test-write");
+        exit(1);
+    }
+
     close(test_io_file_write_fd);
 }
 
@@ -29,9 +32,13 @@ void test_read(void)
 {
     char test_output[TEST_DATA_SIZE];
     int test_io_file_read_fd = open(TEST_FILE, O_RDONLY);
-    printf("Attempting to read <%d> bytes\n", TEST_DATA_SIZE);
+
     int rbytes = read(test_io_file_read_fd, test_output, TEST_DATA_SIZE);
-    printf("<%d> read\n", rbytes);
+    if (rbytes < 0) {
+        perror("test-read");
+        exit(1);
+    }
+
     close(test_io_file_read_fd);
 }
 
@@ -49,6 +56,32 @@ void print_taskstats(struct taskstats *taskstats_obj)
                                                   taskstats_obj->write_bytes);
 }
 
+char *get_procfs_io_stats(pid_t pid)
+{
+    char procfs_io_path[256];
+    snprintf(procfs_io_path, 255, "/proc/%d/io", pid);
+
+    int open_fd = open(procfs_io_path, O_RDONLY);
+    if (open_fd < 0) {
+        perror("proc-open");
+        exit(1);
+    }
+
+    char *io_buf = malloc(1024);
+
+    int rbytes = read(open_fd, io_buf, 1023);
+    if (rbytes < 0) {
+        perror("proc-read");
+        exit(1);
+    }
+
+    io_buf[rbytes] = '\0';
+
+    close(open_fd);
+
+    return io_buf;
+}
+
 int main(int argc, char *argv[])
 {
     pid_t test_pid;
@@ -59,16 +92,39 @@ int main(int argc, char *argv[])
         test_pid = strtol(argv[1], NULL, 0);
     else {
         test_pid = getpid();
-        printf("Process write\n");
         test_write();
-        printf("Process read\n");
         test_read();
     }
 
-    printf("Test process id: <%d>\n", test_pid);
+    char *io = get_procfs_io_stats(test_pid);
     taskstats_obj = get_taskstats(taskobj, test_pid);
+
     assert(taskstats_obj->ac_pid == test_pid);
+
+    printf("Test process id: <%d>\n", test_pid);
     print_taskstats(taskstats_obj);
+
+    /*
+     * The procfs file /proc/$PID/io has matching fields for i/o to some of the 
+     * fields for input output accounting in taskstats.  There can appear to be
+     * some discrepancies at first glance but they explained by a couple of
+     * factors:
+     *
+     * The "syscr" and "syscw" in taskstats are both rounded off by & ~(1024 - 1)
+     * 
+     * The "rchar" and "wchar" in /proc/$PID/io are both affected by *any* 
+     * read/write that the process caused.  Which includes any terminal i/o and 
+     * for reading any shared objects (whether the calls actually reach the disk 
+     * or not). Running strace on the test you will see ~832 bytes read off of 
+     * libtask.so, this matches up with the difference between procfs-io and 
+     * taskstats.
+     *
+     * The "read_bytes" and "write_bytes" are the fields in procfs-io which 
+     * reflect the *actual* number of bytes the process did cause
+     */
+
+    printf("%s", io);
+    free(io);
 
     unlink(TEST_FILE);
     FREE_TASK(taskobj);
